20da839d98c9220f808ae33e0cfb5a18e50307cb
HalilMese <kariruyt@gmail.com>
2026-05-24 01:28:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log(
+"HIM PC Builder loaded 🔥"
+);
+
+/* SCROLL ANIMATION */
+
+const cards =
+
+document.querySelectorAll(
+".build-card, .hardware-card"
+);
+
+window.addEventListener(
+"scroll",
+() => {
+
+cards.forEach(card => {
+
+const top =
+card.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 50){
+
+card.style.opacity = "1";
+
+card.style.transform =
+"translateY(0)";
+
+}
+
+});
+
+});
+
+cards.forEach(card => {
+
+card.style.opacity = "0";
+
+card.style.transform =
+"translateY(40px)";
+
+card.style.transition =
+"0.6s ease";
+
+});
